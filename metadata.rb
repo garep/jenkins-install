cfd0efbabd63c446c84c89909effeaaa8b9fa49e
Gary Phelps <gary.phelps@hp.com>
2014-10-13 09:35:57 -0700

    first commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'jenkins-install'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures jenkins-install'
+long_description 'Installs/Configures jenkins-install'
+version          '0.1.0'
+
